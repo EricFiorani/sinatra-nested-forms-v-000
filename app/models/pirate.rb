6435969b9dbdf9861e133aed3aa764ff56c076fc
Eric Fiorani <efiorani305@gmail.com>
2018-08-24 15:02:15 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -2,17 +2,17 @@ class Pirate
 
   attr_reader :name, :weight, :height
 
-@@all= []
+  @@pirates= []
 
   def initialize(params)
     @name = params[:name]
     @weight = params[:weight]
     @height = params[:height]
-    @@all << self
-  end@@all
+    @@pirates << self
+  end@@pirates
 
-  def self.all
-    @@all
+  def self.pirates
+    @@pirates
   end
 
 end
