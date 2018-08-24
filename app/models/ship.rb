22093f24c700249702c2f995d0ba7e0b541210eb
Eric Fiorani <efiorani305@gmail.com>
2018-08-24 14:59:30 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,13 +2,13 @@ class Ship
 
   attr_reader :name, :type, :booty
 
-  @@al = []
+  @@all = []
 
   def initialize(args)
     @name = args[:name]
     @type = args[:type]
     @booty = args[:booty]
-    SHIPS << self
+    @@all << self
   end
 
   def self.all
