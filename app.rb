cac0255cb040c73cb8531cbb3ac18ebeeba23602
Eric Fiorani <efiorani305@gmail.com>
2018-08-24 15:14:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,7 +10,10 @@ module FormsLab
 
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
     end
-      
+
   end
 end
